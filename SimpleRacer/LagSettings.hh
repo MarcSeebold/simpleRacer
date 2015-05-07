584966658841b1c028d73893c8696c4c251802d8
Marc Seebold <marc.seebold@rwth-aachen.de>
2015-05-07 13:56:30 +0200

    Added additional lag settings

--- SimpleRacer/LagSettings.hh
+++ SimpleRacer/LagSettings.hh
@@ -3,12 +3,29 @@
 namespace lagSettings
 {
 // Latencies in seconds
-static float latencyServerToClient = 100/*ms*/ / 1000.f;
-static float latencyClientToServer = 100/*ms*/ / 1000.f;
+static float latencyServerToClient = 500 /*ms*/ / 1000.f;
+static float latencyClientToServer = 500 /*ms*/ / 1000.f;
 // Client-side
 static bool clientSidePhysics = true;
 static bool shortCircuiting = true;
-static bool clientSideInterpolation = true; // TODO: implement
+static bool clientSideInterpolation = false; // TODO: implement
 // Server-side
 static bool serverSideLagCompensation = true; // TODO: implement
+// Other
+/// Time interval for offset corrections in seconds
+static float clientSideInterpolationInterval = 1000 /*ms*/ / 1000.f;
+
+// supress defined but not used warnings
+static void func()
+{
+   (void)&latencyServerToClient;
+   (void)&latencyClientToServer;
+   (void)&clientSidePhysics;
+   (void)&shortCircuiting;
+   (void)&clientSideInterpolation;
+   (void)&latencyServerToClient;
+   (void)&serverSideLagCompensation;
+   (void)&clientSideInterpolationInterval;
+   (void)&func;
+}
 }
