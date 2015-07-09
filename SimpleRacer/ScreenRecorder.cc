aee8d4a9862bfb54843a7de72d61b20ec765f62d
Marc Seebold <marc.seebold@rwth-aachen.de>
2015-07-09 13:21:52 +0200

    Made a class for ScreenRecording

--- SimpleRacer/ScreenRecorder.cc
+++ SimpleRacer/ScreenRecorder.cc
@@ -1,27 +1,95 @@
 #include "ScreenRecorder.hh"
+#include <QProcess>
+#include <QApplication>
+#include <QDesktopWidget> // get screen size
 
 ScreenRecorder::ScreenRecorder(QObject *parent) : QObject(parent)
 {
-   //TODO: IMPLEMENT
+   // TODO: IMPLEMENT
 }
 
 ScreenRecorder::~ScreenRecorder()
 {
-   //TODO: IMPLEMENT
+   // TODO: IMPLEMENT
 }
 
-void ScreenRecorder::startRecording(const QString &_filename)
+void ScreenRecorder::startRecording(const QString &_filename, bool _waitForStarted)
 {
-   //TODO: IMPLEMENT
+   stopRecording();
+   mWorker.reset(new ScreenRecordWorker(_filename));
+   connect(mWorker.get(), &ScreenRecordWorker::finished, this, &ScreenRecorder::workerFinished);
+   connect(mWorker.get(), &ScreenRecordWorker::recordingStarted, this, &ScreenRecorder::workerStarted);
+   mWorker->start();
+   while(_waitForStarted && !isRecording())
+   {
+      qApp->processEvents();
+      QThread::msleep(10);//NOP
+   }
 }
 
-void ScreenRecorder::stopRecording()
+void ScreenRecorder::stopRecording(bool _waitForStopped)
 {
-   //TODO: IMPLEMENT
+   if (!isRecording())
+      return;
+   SR_ASSERT(mWorker != nullptr && "Running but worker is null.");
+   mWorker->requestInterruption();
+   while(_waitForStopped && isRecording())
+   {
+      qApp->processEvents();
+      QThread::msleep(10);//NOP
+   }
 }
 
 bool ScreenRecorder::isRecording() const
 {
-   //TODO: IMPLEMENT
+   return mWorkerRunning;
 }
 
+void ScreenRecorder::workerFinished()
+{
+   SR_ASSERT(true == mWorkerRunning && "Worker not running");
+   mWorkerRunning = false;
+   emit stoppedRecording();
+}
+
+void ScreenRecorder::workerStarted()
+{
+   SR_ASSERT(false == mWorkerRunning && "Worker already running");
+   mWorkerRunning = true;
+}
+
+
+ScreenRecordWorker::ScreenRecordWorker(const QString &_filename) : mFilename(_filename)
+{
+}
+
+void ScreenRecordWorker::run()
+{
+   const _ rec = QApplication::desktop()->screenGeometry();
+   const QString screenSize = QString::number(rec.width()) + "x" + QString::number(rec.height());
+   const QString program = "ffmpeg";
+   const QStringList args = {"-video_size", screenSize, "-framerate",  "60", "-f", "x11grab", "-i", ":0.0", "-c:v", "libx264", "-preset", "superfast", "-y", mFilename};
+   QProcess proc;
+   proc.start(program, args);
+   if (!proc.waitForStarted())
+   {
+      SR_ASSERT(0 && "Failed to start ffmpeg");
+      return;
+   }
+   emit recordingStarted();
+   forever
+   {
+      if (isInterruptionRequested())
+      {
+         proc.write("q"); // "q" quits ffmpeg
+         proc.closeWriteChannel();
+         if (!proc.waitForFinished())
+         {
+            SR_ASSERT(0 && "Failed to quit ffmpeg");
+            return;
+         }
+         return;
+      }
+      msleep(100); //NOP
+   }
+}
--- SimpleRacer/ScreenRecorder.hh
+++ SimpleRacer/ScreenRecorder.hh
@@ -1,7 +1,15 @@
 #pragma once
 
+#include "Common.hh"
 #include <QObject>
+#include <QThread>
 
+SHARED(class, ScreenRecordWorker);
+
+/**
+ * @brief The ScreenRecorder class performs screen capturing
+ * @todo Currently only the first monitor is caputured
+ */
 class ScreenRecorder : public QObject
 {
    Q_OBJECT
@@ -10,13 +18,49 @@ public:
    ~ScreenRecorder();
 
    /// Start recording the screen
-   void startRecording(const QString &_filename);
+   /// @param _waitForStarted If true, this function will block until recording is started.
+   ///        Otherwise, recording may start some time after this function was called.
+   ///        If this is the case please use the signal startedRecording.
+   void startRecording(const QString &_filename, bool _waitForStarted = true);
+
    /// Stops recording the screen
-   void stopRecording();
+   /// @param _waitForStopped If true, this function will block until the recording is stopped
+   ///        Otherwise, recording may continue for some time after this function is called.
+   ///        Please use the stoppedRecording signal then.
+   void stopRecording(bool _waitForStopped = true);
+
    /// Returns true iff screen recording is active
    bool isRecording() const;
 
 signals:
+   /// Emitted when the recording has stopped
+   void stoppedRecording();
+   /// Emitted when the recording has started
+   void startedRecording();
+
+private slots:
+   void workerFinished();
+   void workerStarted();
+
+private:
+   bool mWorkerRunning = false;
+   UniqueScreenRecordWorker mWorker;
+};
+
+class ScreenRecordWorker : public QThread
+{
+   Q_OBJECT
+
+public:
+   ScreenRecordWorker(const QString &_filename);
 
 public slots:
+   /// Main loop
+   void run() override;
+
+signals:
+   void recordingStarted();
+
+private:
+   QString mFilename;
 };
